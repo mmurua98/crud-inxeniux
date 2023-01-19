4eb71f7e48977b95951062b14a2b55e01416cc71
mmurua98 <55469486+mmurua98@users.noreply.github.com>
2023-01-19 09:27:16 -0700

    project

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Create from '../components/Create.vue'
+import Update from '../components/Update.vue'
+import List from '../components/List.vue'
+
+//Rutas
+const routes = [
+  {
+    path: '/',
+    name: 'List',
+    component: List
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
+  },
+  {
+    path: '/update/:id',
+    name: 'Update',
+    component: Update
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
